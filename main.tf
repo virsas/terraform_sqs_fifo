a21ea1d79d222bdd94c93f2268189770c2625854
Stefan Moravcik <stefan@virsas.com>
2022-03-07 19:50:28 +0100

    fixed the kms variable usage

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_sqs_queue" "deadletter_queue" {
   name                       = "${var.instance.name}_deadletter.fifo"
   fifo_queue                 = true
 
-  kms_master_key_id          = var.instance.kms
-  
+  kms_master_key_id          = var.kms
+
   message_retention_seconds  = 1209600
 }
 
@@ -12,7 +12,7 @@ resource "aws_sqs_queue" "queue" {
   fifo_queue                 = true
   max_message_size           = var.instance.message_size
 
-  kms_master_key_id          = var.instance.kms
+  kms_master_key_id          = var.kms
 
   message_retention_seconds  = var.instance.retention_time
   visibility_timeout_seconds = var.instance.visibility_time
